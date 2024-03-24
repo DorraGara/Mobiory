4307243138591e08f43ef8c0d0dbf735e96f941b
DorraGara <dorra.gara@insat.u-carthage.tn>
2024-03-24 17:05:45 +0100

    Underline title in HomeScreen

--- app/src/main/java/com/example/mobiory/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mobiory/ui/screens/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -64,7 +65,10 @@ fun EventOfTheDay() {
     ) {
         Text(
             text = "Event of the day",
-            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
+            style = TextStyle(
+                textDecoration = TextDecoration.Underline,
+                fontWeight = FontWeight.Bold, fontSize = 24.sp
+            ),
             modifier = Modifier.padding(bottom = 16.dp)
         )
         if (event != null) {
